a7ef19a50e6e1537e8f076732236838b462a0076
charlie <ca.leduc@me.com>
2020-11-22 19:53:16 +0100

    Version 3.0.1
    - Updated code for Vue 3.x

--- README.md
+++ README.md
@@ -13,30 +13,70 @@ npm install vue-scroll2 --save
 Default use in your main.js Vue project
 
 ```javascript
-import ScrollTo from 'vue-scroll2'
-
-Vue.use(ScrollTo)
+import { plugin as ScrollTo } from 'vue-scroll2'
+...
+createApp(App)
+  .use(ScrollTo)
+...
 ```
 
 You can pass an optional object for default duration and easing function
 
 ```javascript
-Vue.use(ScrollTo, { easing: 'linear', duration: 600 })
+...
+createApp(App)
+  .use(ScrollTo, { easing: 'linear', duration: 600 })
+...
 ```
 
-Use the instance methods 'this.$scrollTo' in your Vue script
+Use the instance methods `this.$scrollTo` in your Vue script
 
 ```javascript
 ...
 // e.g. Instance method @click="goTo('my-id')" in your html template
-goTo: function(anchor) {
-  // Scroll to the corresponding html element id with defaults
-  this.$scrollTo(anchor)
-  // Or scroll with optional parameters, offset can be computed to match a fixed header element height for instance
+goTo(anchor) {
+  // Scroll to the corresponding html element id with optional parameters, offset can be computed to match a fixed header element height for instance
   this.$scrollTo(anchor, { easing: 'linear', duration: 600, offset: 32 })
 }
 ...
 ```
+
+## Standalone
+
+You can import `scrollTo` in your Vue script to use it as a standalone function.
+
+```javascript
+import { scrollTo } from 'vue-scroll2'
+export default {
+  name: 'MyComponent',
+
+  methods: {
+    goTo(anchor) {
+      const easeInQuad = (t) => {
+        return t * t
+      }
+      // You can pass a custom function with t parameters
+      scrollTo(anchor, { easing: myCustomFn })
+    }
+  }
+}
+```
+## List of available easing functions
+
+By default, the plugin use a ease-in-out cubic function but you can pass one of the following parameters:
+- 'linear'
+- 'easeInQuad'
+- 'easeOutQuad'
+- 'easeInOutQuad'
+- 'easeInCubic'
+- 'easeOutCubic'
+- 'easeInOutCubic'
+- 'easeInQuart'
+- 'easeOutQuart'
+- 'easeInOutQuart'
+- 'easeInQuint'
+- 'easeOutQuint'
+- 'easeInOutQuint'
 
 ## License
 
--- index.js
+++ index.js
@@ -1,62 +1,166 @@
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
 /**
  * A lightweight smooth scroll plugin for Vue.js
  *
- * @version 0.2.1
+ * @version 3.0.1
  * @author Charlie LEDUC <contact@graphique.io>
  * @license ISC
  * @requires 'vue'
  */
 
-import easing from './easing';
+var _SCROLLTO_DURATION_ = 500;
+var _SCROLLTO_EASING_ = 'easeInOutCubic';
 
-export default {
-  install: function install(Vue, options) {
-    var defaultEasingFn = easing.get('easeInOutQuad');
-    var defaultDuration = 500;
-    if (options) {
-      if (typeof options.easing === 'string') {
-        defaultEasingFn = easing.get(options.easing);
+function _getEasing(fn) {
+  switch (fn) {
+    case 'linear':
+      {
+        // no easing, no acceleration
+        return function linear(t) {
+          return t;
+        };
       }
-      if (options.duration) {
-        defaultDuration = Number.parseInt(options.duration);
+    case 'easeInQuad':
+      {
+        // accelerating from zero velocity
+        return function easeInQuad(t) {
+          return t * t;
+        };
       }
+    case 'easeOutQuad':
+      {
+        // decelerating to zero velocity
+        return function easeOutQuad(t) {
+          return t * (2 - t);
+        };
+      }
+    case 'easeInOutQuad':
+      {
+        // acceleration until halfway, then deceleration
+        return function easeInOutQuad(t) {
+          return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+        };
+      }
+    case 'easeInCubic':
+      {
+        // accelerating from zero velocity
+        return function easeInCubic(t) {
+          return t * t * t;
+        };
+      }
+    case 'easeOutCubic':
+      {
+        // decelerating to zero velocity
+        return function easeOutCubic(t) {
+          return --t * t * t + 1;
+        };
+      }
+    case 'easeInOutCubic':
+      {
+        // acceleration until halfway, then deceleration
+        return function easeInOutCubic(t) {
+          return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
+        };
+      }
+    case 'easeInQuart':
+      {
+        // accelerating from zero velocity
+        return function easeInQuart(t) {
+          return t * t * t * t;
+        };
+      }
+    case 'easeOutQuart':
+      {
+        // decelerating to zero velocity
+        return function easeOutQuart(t) {
+          return 1 - --t * t * t * t;
+        };
+      }
+    case 'easeInOutQuart':
+      {
+        // acceleration until halfway, then deceleration
+        return function easeInOutQuart(t) {
+          return t < 0.5 ? 8 * t * t * t * t : 1 - 8 * --t * t * t * t;
+        };
+      }
+    case 'easeInQuint':
+      {
+        // accelerating from zero velocity
+        return function easeInQuint(t) {
+          return t * t * t * t * t;
+        };
+      }
+    case 'easeOutQuint':
+      {
+        // decelerating to zero velocity
+        return function easeOutQuint(t) {
+          return 1 + --t * t * t * t * t;
+        };
+      }
+    case 'easeInOutQuint':
+      {
+        // acceleration until halfway, then deceleration
+        return function easeInOutQuint(t) {
+          return t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * --t * t * t * t * t;
+        };
+      }
+    default:
+      {
+        // no easing, no acceleration
+        return function linear(t) {
+          return t;
+        };
+      }
+  }
+}
+
+function _scrollTo(id, params) {
+  if (id == null) return;
+  var element = document.getElementById(id);
+  if (element == null) return;
+
+  var offset = 0;
+  var duration = _SCROLLTO_DURATION_;
+  var easingFn = typeof _SCROLLTO_EASING_ === 'function' ? _SCROLLTO_EASING_ : _getEasing(_SCROLLTO_EASING_);
+  if (params) {
+    if (params.duration) duration = params.duration;
+    if (params.easing) {
+      easingFn = params.easing && typeof params.easing === 'function' ? params.easing : _getEasing(params.easing);
     }
+    if (params.offset) offset = params.offset;
+  }
 
-    Vue.prototype.$scrollTo = function (query, params) {
-      if (query == null) return;
-      var element = document.getElementById(query);
-      if (element == null) return;
+  var startingY = window.pageYOffset;
+  var elementY = window.pageYOffset + (element.getBoundingClientRect().top - offset);
 
-      var offset = 0;
-      var duration = defaultDuration;
-      var easingFn = defaultEasingFn;
-      if (params) {
-        if (params.duration) duration = params.duration;
-        if (params.easing) easingFn = easing.get(params.easing);
-        if (params.offset) offset = params.offset;
-      }
+  var targetY = document.body.scrollHeight - elementY < window.innerHeight ? document.body.scrollHeight - window.innerHeight : elementY;
+  var diff = targetY - startingY;
+  var start;
 
-      var startingY = window.pageYOffset;
-      var elementY = window.pageYOffset + (element.getBoundingClientRect().top - offset);
+  if (!diff) return;
 
-      var targetY = document.body.scrollHeight - elementY < window.innerHeight ? document.body.scrollHeight - window.innerHeight : elementY;
-      var diff = targetY - startingY;
-      var start;
+  window.requestAnimationFrame(function step(timestamp) {
+    if (!start) start = timestamp;
+    var time = timestamp - start;
+    var percent = Math.min(time / duration, 1);
+    percent = easingFn(percent);
 
-      if (!diff) return;
+    window.scrollTo(0, startingY + diff * percent);
 
-      window.requestAnimationFrame(function step(timestamp) {
-        if (!start) start = timestamp;
-        var time = timestamp - start;
-        var percent = Math.min(time / duration, 1);
-        percent = easingFn(percent);
+    if (time < duration) {
+      window.requestAnimationFrame(step);
+    }
+  });
+}
 
-        window.scrollTo(0, startingY + diff * percent);
-
-        if (time < duration) {
-          window.requestAnimationFrame(step);
-        }
-      });
-    };
+var _plugin = {
+  install: function install(app, options) {
+    if (!options || (typeof options === 'undefined' ? 'undefined' : _typeof(options)) !== 'object') options = {};
+    _SCROLLTO_DURATION_ = Number.parseInt(options.duration || 500);
+    _SCROLLTO_EASING_ = options.easing || 'easeInOutCubic';
+    app.config.globalProperties.$scrollTo = _scrollTo;
   }
 };
+
+export { _scrollTo as scrollTo, _plugin as plugin };
